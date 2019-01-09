7f9596ef4d5529e898a1759e8ecec3b0f361896e
Francisco Pires <afonso.fcul@gmail.com>
2019-01-09 11:39:46 +0000

    use DjangoJSONEncoder (WIP, not tested)
    This encoder prevents convering special cases (like uuid and datetime formats)

--- django_splunk_logging/event.py
+++ django_splunk_logging/event.py
@@ -1,47 +1,37 @@
 import datetime
 import json
 import logging
-import os
-import time
 from threading import Thread
-from uuid import UUID
 
 import requests
 from django.conf import settings
+from django.core.serializers.json import DjangoJSONEncoder
 
 from .utils import _get_request
 
+LOGGER = logging.getLogger(__name__)
 
-class SplunkEvent(object):
-    _key = None
-    _timestamp = None
-    _request = None
-    _user = None
-    _auth = None
-    _start = None
-    _obj = None
-    _name = None
-    _auth_key = "Splunk {0}".format(settings.SPLUNK_TOKEN)
+
+class SplunkEvent:
 
     def __init__(self, *args, **kwargs):
         if not settings.SPLUNK_LOGS:
             return
         self._key = kwargs.pop('key', "Generic")
-        self._timestamp = str(time.time())
+        self._timestamp = str(datetime.utcnow())
         self._request = kwargs.pop('request', _get_request())
         self._user = kwargs.pop('user', None)
         self._name = kwargs.pop('name', None)
         self._obj = kwargs.pop('obj', None)
 
-        if self._request is not None:
+        if self._request:
             try:
                 self._auth = self._request.user.is_authenticated()
                 self._user = self._request.session.get('user_id', None)
             except:
                 self._auth = False
 
-        ran_shortcut = self.package_obj(self._obj)
-        if ran_shortcut:
+        if self.package_obj(self._obj):
             if settings.SPLUNK_THREAD_EVENTS:
                 Thread(target=self.send_to_splunk).start()
             else:
@@ -52,46 +42,37 @@ class SplunkEvent(object):
         Shortcut method if an object is passed to the init method.
 
         Generally used for objects that have a to_json() method.
+
+        If list of objects, handle it in self.format()
         """
-        if obj is None:
+        if not obj:
             return False
+
         if isinstance(obj, list):
-            ## if it is a list of objects, handle it in self.format()
             return True
 
         if 'to_json' in dir(obj):
             for k, v in obj.to_json().items():
                 setattr(self, k, v)
 
-        elif isinstance(obj, dict):
+        elif issubclass(obj, dict):
             for key, value in obj.items():
-                if type(value) is datetime.datetime:
-                    setattr(self, key, value.strftime('%m/%d/%Y %H:%M:%S'))
-                elif type(value) is UUID:
-                    setattr(self, key, str(value))
-                else:
-                    setattr(self, key, value)
+                setattr(self, key, value)
         else:
             for oa in [x for x in obj.__dict__ if not x.startswith('_')]:
-                if type(getattr(obj, oa)) is datetime.datetime:
-                    setattr(self,
-                            oa,
-                            getattr(obj, oa).strftime('%m/%d/%Y %H:%M:%S'))
-                elif type(getattr(obj, oa)) is UUID:
-                    setattr(self, oa, str(getattr(obj, oa)))
-                else:
-                    setattr(self, oa, getattr(obj, oa))
+                setattr(self, oa, getattr(obj, oa))
+
         return True
 
     def send_to_splunk(self):
         url = f'{settings.SPLUNK_URL}:{settings.SPLUNK_EC_PORT}/services/collector/event'
         headers = {'Authorization': f'Splunk {settings.SPLUNK_TOKEN}'}
-        data = json.dumps(self.format())
+        data = json.dumps(self.format(), cls=DjangoJSONEncoder)
 
         r = requests.post(url, headers=headers, data=data, verify=False)
 
-        if r.status_code not in range(200, 300):
-            LOGGER.debug(f'SplunkHandler: error sending splunk event to http collector: {r.text}')
+        if r.status_code >= 300:
+            LOGGER.debug(f'SplunkEvent: error sending splunk event to http collector: {r.text}')
 
     def format_request(self):
         """ Format the request to JSON. """
@@ -134,35 +115,33 @@ class SplunkEvent(object):
                 elif self._request.method == "POST":
                     data['POST'] = self._request.POST
             except Exception as e:
-                pass
+                LOGGER.debug(f'SplunkEvent: {e}')
             return data
 
     def format(self):
-        """ Format the SplunkEvent to JSON. """
+        """
+        Format the SplunkEvent to JSON.
+
+        subclass(o, dict): checking subclass helps with cases like defaultdict and OrderedDict
+        """
         if isinstance(self._obj, list):
-            # list of objects
-            event_obj = []
+            list_obj = []
             for o in self._obj:
                 item = {}
                 if 'to_json' in dir(o):
                     item = o.to_json()
-                elif isinstance(o, dict):
+                elif issubclass(o, dict):
                     item = o
                 else:
                     for oa in [x for x in o.__dict__ if not x.startswith('_')]:
-                        if type(getattr(o, oa)) is datetime.datetime:
-                            item[oa] = getattr(o, oa).strftime(
-                                '%m/%d/%Y %H:%M:%S')
-                        elif type(getattr(o, oa)) is UUID:
-                            item[oa] = str(getattr(o, oa))
-                        else:
-                            item[oa] = getattr(o, oa)
-                event_obj.append(item)
+                        item[oa] = getattr(o, oa)
+                list_obj.append(item)
 
         else:
             event_obj = {}
             for x in [attr for attr in self.__dict__ if not attr.startswith('_')]:
                 event_obj[x] = getattr(self, x)
+
         data = {}
         data['time'] = self._timestamp
         data['sourcetype'] = self._key
@@ -174,14 +153,3 @@ class SplunkEvent(object):
             'event': self._name,
         }
         return data
-
-    def start_timer(self):
-        """ Start a Timer. """
-        self._start = time.time()
-
-    def stop_timer(self):
-        """ Stop the Timer and assign value to object. """
-        try:
-            self.execution = int(round((time.time() - self._start)*1000))
-        except AttributeError:
-            logging.error('you didnt start the timer!')
